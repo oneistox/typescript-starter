72bf7c3c3398483b684eb45469f87f9f81d3ca7c
janavdhingra <janavdhingra11@gmail.com>
2022-06-26 23:17:18 +0530

    first commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { promises as fs, writeFile,  } from 'fs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  async readFile(): Promise<string> {
+    const readText = await fs.readFile('../files/s1.txt', 'utf8');
+    return readText;
   }
+
+  async writeFile(): Promise<string>{
+    const editedFile = await fs.writeFile('../files/s1.txt', 'hey');
+    const readText = await fs.readFile('../files/s1.txt', 'utf8');
+    return readText;
+  }
+
+  async createFile(): Promise<File>{
+    var file = new File(["s2"], "s2.txt", {type: "text/plain",});
+    return file;
+  }
+
 }
+
+
